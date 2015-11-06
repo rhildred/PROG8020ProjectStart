23d14b6ce4f84a6bc92d814d4edbf536464353d3
rhildred <rhildred@gmail.com>
2015-11-06 10:17:00 -0500

    added product page

--- www/index.php
+++ www/index.php
@@ -2,6 +2,9 @@
 
 require __DIR__ . "/../vendor/autoload.php";
 
+//connect to database
+$oDb = new PDO("sqlite:" . __DIR__ . "/../products.sqlite");
+
 $oApp = new \Slim\Slim(array(
         'view' => new \PHPView\PHPView(),
         'templates.path' => __DIR__ . "/../views" ));
@@ -16,6 +19,23 @@ $oApp->get("/about", function()use($oApp){
 
 $oApp->get("/contact", function()use($oApp){
    $oApp->render("contact.phtml"); 
+});
+
+$oApp->get("/products/:productID", function($nId) use($oApp, $oDb){
+    
+    // fetching product
+    $oStmt = $oDb->prepare("SELECT * FROM products WHERE productID = :id");
+    $oStmt->bindParam("id", $nId);
+    $oStmt->execute();
+    $aProducts = $oStmt->fetchAll(PDO::FETCH_OBJ);
+    
+    //fetching images
+    $oStmt = $oDb->prepare("SELECT * FROM images WHERE productID = :id");
+    $oStmt->bindParam("id", $nId);
+    $oStmt->execute();
+    $aImages = $oStmt->fetchAll(PDO::FETCH_OBJ);
+
+    $oApp->render("product.phtml", array("product"=>$aProducts[0], "images"=>$aImages)); 
 });
 
 
